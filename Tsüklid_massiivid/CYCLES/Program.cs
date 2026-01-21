891cd889b64b0f0f3d2eccb64df22518669e2f62
Opilane TTHK <opilane@tthk.ee>
2026-01-21 10:00:22 +0200

    meetod1

--- Tsüklid_massiivid/CYCLES/Program.cs
+++ Tsüklid_massiivid/CYCLES/Program.cs
@@ -1,164 +1,244 @@
-﻿namespace CYCLES
+﻿//namespace CYCLES
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            /* Do-while ülesanded*/
+
+//            // 1. "arvuta kuni summani"
+//            // tee muutuja "täisarv" mille sees on täisarv 0
+//            // kirjuta do while tsükkel
+//            // tsükli tegevusena lahuta muutujast "täisarv" maha 2
+//            // ja kuva muutuja "täisarv" välja
+//            // -> do while tsükli tingimuseks kirjuta kontroll, mis kontrollib kas muutujas "täisarv" on arv miinuses rohkem kui -31
+//            // peale tsüklit kuva sõnum "tsükkel lõppes"
+
+//            // 2. "pikenda sõnumit"
+//            // tee muutuja "sõnum" mille sees on tühi string, nt ""
+//            // kirjuta do while tsükkel
+//            // tsükli tegevusena küsi kasutajalt sõnumile lisa "palun lisa järgmine sõnumi osa
+//            // lisa muutujasse "sõnum" kasutaja poolt tehtud sisestus
+//            // kuva muutuja "sõnum" välja
+//            // -> do while tsükli tingimuseks kirjuta kontroll, mis kontrollib muutuja "sõnum" pikkust .Length abil, kui sõnum on pikem kui 140 tähte, siis tsükkel lõppeb
+//            // peale tsüklit kuva sõnum "see sõnum enam twitterisse ei mahuks, kirjuta vähem fam"
+
+//            int täisArv = 0;
+//            do
+//            {
+//                täisArv -= 2;
+//                Console.WriteLine(täisArv);
+//            }
+//            while (täisArv > -31);
+//            Console.WriteLine("tsükkel lõppes");
+
+
+//            /* While ülesanded*/
+
+//            // 1. "niikaua kuni ohtu pole"
+//            // tee muutuja "CO2tase" mille sees on komakohaga float 0.0f
+//            // kirjuta while tsükkel
+//            // -> while tsükli tingimuseks kirjuta kontroll, mis kontrollib kas muutujas "CO2tase" on arv väiksem kui 40.000
+//            // tsükli tegevusena kuva sõnum "mis on co2 tase?"
+//            // pane muutujasse "CO2tase" info kasutajalt
+//            // tee tingimuslauseline kontroll, mis kontrollib, kui kasutaja sisestus on väiksem kui 40.000,
+//            // siis kuva sõnum kasutajale teavitades, et otsest ohtu ei ole
+//            // peale tsüklit kuva sõnum "evakueeri hoonest, süsihappegaasi tase on liiga kõrgel"
+//            /* 
+//             * NB programm peab niikaua käima kuni kasutaja sisestus on suurem või võrdne kui 40.000
+//             */
+
+//            float CO2tase = 0.0f;
+//            Console.WriteLine("Mis on CO2 tase?");
+//            CO2tase = float.Parse(Console.ReadLine());
+//            while (CO2tase <= 40.000f)
+//            {
+//                Console.WriteLine("Otsest ohtu ei ole");
+//                Console.WriteLine("Miso on CO2 tase?");
+//                CO2tase = float.Parse(Console.ReadLine());
+//            }
+//            if (CO2tase >= 40.000f)
+//            {
+//                Console.WriteLine("evakuueri hoonest süsihappe on liiga kõrgel");
+//            }
+
+
+
+
+//            // 2. "vibumees"
+//            // tee muutuja "mehike" mille sees on täisarv 0
+//            // tee muutuja "vastus" mille sees on tühi tekst
+//            // kirjuta while tsükkel
+//            // -> while tsükli tingimuseks kirjuta kontroll:
+//            //      mis kontrollib kas muutujas "mehike" on täisarv 5 või vähem
+//            //      või
+//            //      muutujas "vastus" on tekst "jah"
+//            // tsükli tegevuseks küsi kasutajalt "kas tahad mehikesse noole lasta?"
+//            // tsükli tegevuseks pane muutujasse "vastus" väärtus kasutajalt käsurea pealt 
+//            // tsükli tegevuseks kontrolli tingimuslausega kas vastus oli "jah"
+//            // tsükli tegevuseks Kui tingimuslause vastus oli jah, siis:
+//            // kuva kasutajale tekst "mitu noolt lased?"
+//            // lisa muutujasse "mehike" kasutajalt saadud täisarv
+//            // peale tsüklit kontrolli kas muutujas "mehike" olev arv on väiksem kui 5
+//            // kui on väiksem kui 5 siis kuva sõnum "mehike elab :D"
+//            // kõikidel muudel juhtudel kuva sõnum "tapsid mehikese ära :c"
+
+//            /* For ülesanded*/
+
+//            // 1. "prindi numbrid"
+//            // tee muutuja "kuipalju" milles on täisarv 0 
+//            // kuva kasutajale tekst millega küsid kasutajalt mitu numbrit ta tahab
+//            // omista käsurealt saadud arv muutujasse "kuipalju"
+//            // kirjuta for tsükkel, tsükli teise parameetrisse - kontrolli aseta i vastu muutuja "kuipalju"
+//            // tsükli tegevusena kuva kasutajale välja i, aga liida sellele üks juurde et lugemine algaks arvust 1
+//            // peale tsüklit kuva kasutajale sõnum "tsükkel lõppes"
+
+//            /* K O O D   S I I A  */
+
+//            int kuiPalju = 0;
+//            Console.WriteLine("kui mitu numbrit te tahate?");
+//            kuiPalju = int.Parse(Console.ReadLine());
+//            for (int i =0; i < kuiPalju; i++)
+//            {
+//                Console.WriteLine(i + 1);
+//            }
+//            Console.WriteLine("tsükkel on lõppenud");
+
+//            // 2. "Ruut"
+//            // tee muutuja "ruudukülg" milles on täisarv 0
+//            // kuva kasutajale tekst millega küsid kasutajalt kui suurt ruutu ta tahab
+//            // omista käsurealt saadud arv muutujasse "ruudukülg"
+//            // kirjuta for tsükkel, tsükli tingimuse kontrolli pane i kontrollimisse muutuja "ruudukülg"
+//            // tsükli sisse kirjuta sõne tüüpi muutuja, "seeRida", kuhu omistad tühja sõne.
+//            // tsükli sees on omakorda teine for tsükkel, ka selle tingimuse kontrolli pane i kontrollimisse muutuja "ruudukülg"'
+//            // nüüd sisemise tsükli sees, omista muutujale "seeRida" juurde liites sisse väärtus "HH" 
+//            // kui sisemine tsükkel on lõpetanud, siis kuva kasutajale see rida välja
+//            // kui ka esimene tsükkel on lõpetanud, siis kuva kasutajale tekst "tsüklid on lõpetanud"
+
+//            int ruudukülg = 0;
+//            Console.WriteLine("kui suurt ruutu te tahate");
+//            ruudukülg = int.Parse(Console.ReadLine());
+//            for (int i = 0; i < ruudukülg; i++)
+//            {
+//                string seeRida = "";
+//                for (int j = 0; j < ruudukülg;  j++)
+//                {
+//                    seeRida = seeRida + "HH";
+//                }
+//                Console.WriteLine(seeRida);
+//            }
+//            Console.WriteLine("tsüklid on lõppenud");
+
+
+//            /* Foreach + list ülesanded */
+
+//            // 1. "suured summad"
+//            // kirjuta viie elemendiga massiiv, elemendid on 1,50,10,15,20, muutuja nimi otsusta ise.
+//            // kirjuta foreach tsükkel, ja võta kollektsiooniparameetriks oma viie elemendiga massiivi muutuja nimetus
+//            // tsükli sisse tee tingimuslause, mis kontrollib kas element on suurem kui 15
+//            // kui on suurem, siis kuva see element kasutajale välja
+//            /* vihje, 
+//             * kui programm on õigesti kirjutatud, 
+//             * siis kuvatakse kasutajale välja elemendid kakskümmend ja viiskümmend
+//             */
+//            int[] apelsiniMahl = [1, 50, 10, 15, 20];
+//            foreach (var arv in apelsiniMahl)
+//            {
+//                if (arv > 15)
+//                {
+//                    Console.WriteLine(arv);
+//                }
+//            }
+
+
+
+// 2. "summeerimine"
+// kirjuta viie elemendiga massiiv, elemendid on 343,686,2300,4439,6996, muutuja nimi otsusta ise.
+// kirjuta täisarvu tüüpi muutuja mille sisse omistad väärtuse 0 ning mille nime otsustad ise
+// kirjuta foreach tsükkel, ja võta kollektsiooniparameetriks oma viie elemendiga massiivi muutuja nimetus
+// tsükli sisse liida oma täisarvu tüüpi muutujale elemendi sees olev arv juurde
+// peale tsüklit kuva kasutajale oma täisarvu tüüpi muutuja välja
+
+
+
+
+
+//Console.writeline("tere mis on sinu nimi");
+//string nimi = "";
+//{
+//    if (nimi != "nipitiri") ;
+//}
+//Console.writeline("sa pole nipitiri ootan ainult nipitirit");
+//}
+
+//Console.writeline("tere kasutaja, kas sulle meeldivad tulbid?");
+//string tulbid = "";
+//tulbid = Console.readline();
+//if (tulbid != "jah")
+//{
+//    Console.writeline("mulle ka");
+//}
+//else
+//{
+//    console.writeline("ma ei saa sinust aru");
+//}
+
+//Console.WriteLine("Kui kaua sa oled sa viimasel nädalal mänginud");
+//int tunnid = 0;
+//tunnid = int.Parse(Console.ReadLine());
+
+//if (tunnid == 0);
+//{
+//    Console.WriteLine("Kahju et sul aega mängida pole");
+//}
+//else if (tunnid >= 1)
+//{
+//    Console.WriteLine("mõõdukas mängja oled");
+//}
+//else if (tunnid >= 1 && tunnid <= 10)
+//{
+//    Console.WriteLine("natuke liiga paljuks läheb kodutööd kannatavad");
+//}
+//else if (tunnid >= 10 && tunnid <= 20)
+//{
+//    Console.WriteLine("liiga palju mängid kas sa koolis üldse nii jõuad");
+//}
+//else if (tunnid >= 20 && tunnid <= 40)
+//{
+//    Console.WriteLine("sul on vist vaja psühholoogi");
+//}
+//else if (tunnid >= 20 && tunnid <= 150) ;
+
+
+
+string nimi = "";
+do
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            /* Do-while ülesanded*/
-
-            // 1. "arvuta kuni summani"
-            // tee muutuja "täisarv" mille sees on täisarv 0
-            // kirjuta do while tsükkel
-            // tsükli tegevusena lahuta muutujast "täisarv" maha 2
-            // ja kuva muutuja "täisarv" välja
-            // -> do while tsükli tingimuseks kirjuta kontroll, mis kontrollib kas muutujas "täisarv" on arv miinuses rohkem kui -31
-            // peale tsüklit kuva sõnum "tsükkel lõppes"
-
-            // 2. "pikenda sõnumit"
-            // tee muutuja "sõnum" mille sees on tühi string, nt ""
-            // kirjuta do while tsükkel
-            // tsükli tegevusena küsi kasutajalt sõnumile lisa "palun lisa järgmine sõnumi osa
-            // lisa muutujasse "sõnum" kasutaja poolt tehtud sisestus
-            // kuva muutuja "sõnum" välja
-            // -> do while tsükli tingimuseks kirjuta kontroll, mis kontrollib muutuja "sõnum" pikkust .Length abil, kui sõnum on pikem kui 140 tähte, siis tsükkel lõppeb
-            // peale tsüklit kuva sõnum "see sõnum enam twitterisse ei mahuks, kirjuta vähem fam"
-
-            int täisArv = 0;
-            do
-            {
-                täisArv -= 2;
-                Console.WriteLine(täisArv);
-            }
-            while (täisArv > -31);
-            Console.WriteLine("tsükkel lõppes");
-
-
-            /* While ülesanded*/
-
-            // 1. "niikaua kuni ohtu pole"
-            // tee muutuja "CO2tase" mille sees on komakohaga float 0.0f
-            // kirjuta while tsükkel
-            // -> while tsükli tingimuseks kirjuta kontroll, mis kontrollib kas muutujas "CO2tase" on arv väiksem kui 40.000
-            // tsükli tegevusena kuva sõnum "mis on co2 tase?"
-            // pane muutujasse "CO2tase" info kasutajalt
-            // tee tingimuslauseline kontroll, mis kontrollib, kui kasutaja sisestus on väiksem kui 40.000,
-            // siis kuva sõnum kasutajale teavitades, et otsest ohtu ei ole
-            // peale tsüklit kuva sõnum "evakueeri hoonest, süsihappegaasi tase on liiga kõrgel"
-            /* 
-             * NB programm peab niikaua käima kuni kasutaja sisestus on suurem või võrdne kui 40.000
-             */
-
-            float CO2tase = 0.0f;
-            Console.WriteLine("Mis on CO2 tase?");
-            CO2tase = float.Parse(Console.ReadLine());
-            while (CO2tase <= 40.000f)
-            {
-                Console.WriteLine("Otsest ohtu ei ole");
-                Console.WriteLine("Miso on CO2 tase?");
-                CO2tase = float.Parse(Console.ReadLine());
-            }
-            if (CO2tase >= 40.000f)
-            {
-                Console.WriteLine("evakuueri hoonest süsihappe on liiga kõrgel");
-            }
-
-
-
-
-            // 2. "vibumees"
-            // tee muutuja "mehike" mille sees on täisarv 0
-            // tee muutuja "vastus" mille sees on tühi tekst
-            // kirjuta while tsükkel
-            // -> while tsükli tingimuseks kirjuta kontroll:
-            //      mis kontrollib kas muutujas "mehike" on täisarv 5 või vähem
-            //      või
-            //      muutujas "vastus" on tekst "jah"
-            // tsükli tegevuseks küsi kasutajalt "kas tahad mehikesse noole lasta?"
-            // tsükli tegevuseks pane muutujasse "vastus" väärtus kasutajalt käsurea pealt 
-            // tsükli tegevuseks kontrolli tingimuslausega kas vastus oli "jah"
-            // tsükli tegevuseks Kui tingimuslause vastus oli jah, siis:
-            // kuva kasutajale tekst "mitu noolt lased?"
-            // lisa muutujasse "mehike" kasutajalt saadud täisarv
-            // peale tsüklit kontrolli kas muutujas "mehike" olev arv on väiksem kui 5
-            // kui on väiksem kui 5 siis kuva sõnum "mehike elab :D"
-            // kõikidel muudel juhtudel kuva sõnum "tapsid mehikese ära :c"
-
-            /* For ülesanded*/
-
-            // 1. "prindi numbrid"
-            // tee muutuja "kuipalju" milles on täisarv 0 
-            // kuva kasutajale tekst millega küsid kasutajalt mitu numbrit ta tahab
-            // omista käsurealt saadud arv muutujasse "kuipalju"
-            // kirjuta for tsükkel, tsükli teise parameetrisse - kontrolli aseta i vastu muutuja "kuipalju"
-            // tsükli tegevusena kuva kasutajale välja i, aga liida sellele üks juurde et lugemine algaks arvust 1
-            // peale tsüklit kuva kasutajale sõnum "tsükkel lõppes"
-
-            /* K O O D   S I I A  */
-
-            int kuiPalju = 0;
-            Console.WriteLine("kui mitu numbrit te tahate?");
-            kuiPalju = int.Parse(Console.ReadLine());
-            for (int i =0; i < kuiPalju; i++)
-            {
-                Console.WriteLine(i + 1);
-            }
-            Console.WriteLine("tsükkel on lõppenud");
-
-            // 2. "Ruut"
-            // tee muutuja "ruudukülg" milles on täisarv 0
-            // kuva kasutajale tekst millega küsid kasutajalt kui suurt ruutu ta tahab
-            // omista käsurealt saadud arv muutujasse "ruudukülg"
-            // kirjuta for tsükkel, tsükli tingimuse kontrolli pane i kontrollimisse muutuja "ruudukülg"
-            // tsükli sisse kirjuta sõne tüüpi muutuja, "seeRida", kuhu omistad tühja sõne.
-            // tsükli sees on omakorda teine for tsükkel, ka selle tingimuse kontrolli pane i kontrollimisse muutuja "ruudukülg"'
-            // nüüd sisemise tsükli sees, omista muutujale "seeRida" juurde liites sisse väärtus "HH" 
-            // kui sisemine tsükkel on lõpetanud, siis kuva kasutajale see rida välja
-            // kui ka esimene tsükkel on lõpetanud, siis kuva kasutajale tekst "tsüklid on lõpetanud"
-
-            int ruudukülg = 0;
-            Console.WriteLine("kui suurt ruutu te tahate");
-            ruudukülg = int.Parse(Console.ReadLine());
-            for (int i = 0; i < ruudukülg; i++)
-            {
-                string seeRida = "";
-                for (int j = 0; j < ruudukülg;  j++)
-                {
-                    seeRida = seeRida + "HH";
-                }
-                Console.WriteLine(seeRida);
-            }
-            Console.WriteLine("tsüklid on lõppenud");
-
-
-            /* Foreach + list ülesanded */
-
-            // 1. "suured summad"
-            // kirjuta viie elemendiga massiiv, elemendid on 1,50,10,15,20, muutuja nimi otsusta ise.
-            // kirjuta foreach tsükkel, ja võta kollektsiooniparameetriks oma viie elemendiga massiivi muutuja nimetus
-            // tsükli sisse tee tingimuslause, mis kontrollib kas element on suurem kui 15
-            // kui on suurem, siis kuva see element kasutajale välja
-            /* vihje, 
-             * kui programm on õigesti kirjutatud, 
-             * siis kuvatakse kasutajale välja elemendid kakskümmend ja viiskümmend
-             */
-            int[] apelsiniMahl = [1, 50, 10, 15, 20];
-            foreach (var arv in apelsiniMahl)
-            {
-                if (arv > 15)
-                {
-                    Console.WriteLine(arv);
-                }
-            }
-
-
-
-            // 2. "summeerimine"
-            // kirjuta viie elemendiga massiiv, elemendid on 343,686,2300,4439,6996, muutuja nimi otsusta ise.
-            // kirjuta täisarvu tüüpi muutuja mille sisse omistad väärtuse 0 ning mille nime otsustad ise
-            // kirjuta foreach tsükkel, ja võta kollektsiooniparameetriks oma viie elemendiga massiivi muutuja nimetus
-            // tsükli sisse liida oma täisarvu tüüpi muutujale elemendi sees olev arv juurde
-            // peale tsüklit kuva kasutajale oma täisarvu tüüpi muutuja välja
-
-        }
-    }
+    Console.WriteLine("sisesta oma nimi");
+    nimi = Console.ReadLine();
 }
+while (nimi != "Krissu");
+if (nimi == "Krissu")
+    Console.WriteLine("Tere Krissu");
+
+
+
+int parool = 0;
+Console.WriteLine("Sisesta oma parool");
+parool = int.Parse(Console.ReadLine());
+{
+    Console.WriteLine("parool on vale proovi uuesti");
+    while (parool < 0 && parool > 999) ;
+ 
+    
+    
+}
+while (parool < 9999 && parool > 1000) ;
+parool = int.Parse(Console.ReadLine());
+Console.WriteLine("parool on õige");
+
+
+
+
+
+
