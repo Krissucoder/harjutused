f1fb6399f3aa49b208bff0c1a959a1d9e6bce36f
Opilane TTHK <opilane@tthk.ee>
2026-01-28 09:38:08 +0200

    pliiatsid
    wff

--- IF_loogilisedoperaatorid_tingimus/LOGICOPERATOR/Program.cs
+++ IF_loogilisedoperaatorid_tingimus/LOGICOPERATOR/Program.cs
@@ -1,17 +1,48 @@
-﻿namespace LOGICOPERATOR
+﻿using System.ComponentModel.Design;
+
+namespace LOGICOPERATOR
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //tee neli bool-tüüpi muutujat,
+            bool pliiatsid = false;
+            bool paber = false;
+            bool saag = false;
+            bool lauad = false;
+
+            string sisestus = "";
+            int i = 0;
+            while (i <4)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.WriteLine("kas sul on pliiatsid?");
+                        break;
+                    case 1:
+                        Console.WriteLine("kas sul on paber?");
+                        break;
+                    case 2:
+                        Console.WriteLine("kas sul on saag?");
+                        break;
+                    case 3:
+                        Console.WriteLine("kas sul on lauad?");
+                        break;
+                }
+                while (sisestus != "ei" && sisestus != "ja")
+                {
+                    sisestus = Console.ReadLine();
+                }
+            }
 
             //nimedeks pane neile pliiatsid, paber, saag, lauad
 
             //küsi kasutajalt muutujasse "sisestus" vastus iga objekti
             //kohta küsimus "kas tal on X?" millele kasutaja saab vastata jah või ei
             //küsimine EI PEA olema tsüklis, võib ka teha 4 küsimust järjest
-
+          
             //kui ta on mingi objekti kohta vastanud ei, siis pane vastavasse muutujasse false
             //samamoodi kui ta on mingi objekti kohta vastanud jah, siis pane vastavasse muutujasse true
 
@@ -36,6 +67,22 @@
             //      |   jah     |   ei      |   ei      |   jah     ||  "sul on tööriistad, aga materjali mitte, ma ei oska öelda"
             //      |   ei      |   jah     |   jah     |   ei      ||  "sul on materjal, aga tööriistu mitte, mine k-rautasse"
             //      |  < kõik muud kombinatsoonid tingimustest >    ||  "tühjade kätega ei tee mitte midagi :c"
+            if (!pliiatsid && !paber && lauad && saag)
+            {
+                Console.WriteLine("võiksid õppida tisleriks");
+            }
+            else if (pliiatsid && paber && !lauad && !saag)
+            {
+                Console.WriteLine("võiksid õppida arhitektiks");
+            }
+            else if (!pliiatsid && !paber && lauad && !saag)
+            {
+                Console.WriteLine("võiksid õppida arhitektiks ja tisleriks, sul on kõik uksed avatud");
+            }
+            else if (pliiatsid && paber && lauad && !saag)
+            {
+                Console.WriteLine("sul on tööriistad, aga materjali mitte, ma ei oska öelda");
+            }
         }
     }
 }
